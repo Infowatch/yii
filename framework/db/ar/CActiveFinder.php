3581c9c26cb124da09a03fd9e2282ed47560daa4
gololobov <Igor.Gololobov@infowatch.com>
2015-04-13 16:48:38 +0300

    hinting for select query;

--- framework/db/ar/CActiveFinder.php
+++ framework/db/ar/CActiveFinder.php
@@ -1272,6 +1272,8 @@ class CJoinQuery
 	 */
 	public $elements=array();
 
+	public $hints=array();
+
 	/**
 	 * Constructor.
 	 * @param CJoinElement $joinElement The root join tree.
@@ -1301,6 +1303,9 @@ class CJoinQuery
 			$this->conditions[]=$joinElement->getPrimaryKeyRange();
 		}
 		$this->elements[$joinElement->id]=true;
+
+		if ($criteria!==null && $criteria->hints)
+			$this->hints=array_merge($this->hints,$criteria->hints);
 	}
 
 	/**
@@ -1328,6 +1333,10 @@ class CJoinQuery
 				$this->params=$element->relation->params;
 		}
 		$this->elements[$element->id]=true;
+
+
+		if (!empty($element->relation->hints))
+			$this->hints[]=$element->relation->hints;
 	}
 
 	/**
@@ -1337,7 +1346,18 @@ class CJoinQuery
 	 */
 	public function createCommand($builder)
 	{
-		$sql=($this->distinct ? 'SELECT DISTINCT ':'SELECT ') . implode(', ',$this->selects);
+		$sql='SELECT ';
+		if($this->hints)
+		{
+			$sql.='/*+';
+			foreach($this->hints as $hint)
+			{
+				$sql.=$hint.' ';
+			}
+			$sql.='*/ ';
+		}
+
+		$sql.=($this->distinct?'DISTINCT ':'').implode(', ',$this->selects);
 		$sql.=' FROM ' . implode(' ',array_unique($this->joins));
 
 		$conditions=array();
